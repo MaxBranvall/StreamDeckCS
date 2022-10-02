9c82b07d2ad2afae3a3eb7318cb75bd2ad4d47b5
Max Branvall <maxbranvall@gmail.com>
2022-10-02 08:59:10 -0400

    - Basic bootstrap of plugin backend code

--- SamplePlugin/Program.cs
+++ SamplePlugin/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using StreamDeckCS;
+using StreamDeckCS.EventsReceived;
 
 namespace SamplePlugin
 {
@@ -8,8 +9,32 @@ namespace SamplePlugin
     {
         static async Task Main(string[] args)
         {
-            StreamdeckCore core = new StreamdeckCore(args);
+            SamplePlugin plugin = new SamplePlugin(args);
+            await plugin.startPlugin();
+        }
+    }
+
+    public class SamplePlugin
+    {
+
+        StreamdeckCore core;
+
+        public SamplePlugin(string[] args)
+        {
+            core = new StreamdeckCore(args);
+
+            core.KeyDownEvent += Core_KeyDownEvent;
+        }
+
+        private void Core_KeyDownEvent(object sender, KeyDown e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task startPlugin()
+        {
             await core.Start();
         }
+
     }
 }
